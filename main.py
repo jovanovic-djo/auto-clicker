6371e729c31a9e5c47294c73f4f32fe5e99c4c19
Đorđe Jovanović <119633175+jovanovic-djo@users.noreply.github.com>
2023-03-20 23:36:16 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import time
+import threading
+from pynput.mouse import Controller, Button
+from pynput.keyboard import Listener, KeyCode
+
+### Pressing 'x' will enable and disable auto-clicker
+TOGGLE_KEY = KeyCode(char = "x")
+
+clicking = False
+mouse = Controller()
+
+def clicker():
+    while True:
+        if clicking:
+            mouse.click(Button.left, 1)
+        time.sleep(0.0000001)
+
+
+def toggle_event(key):
+    if key == TOGGLE_KEY:
+        global clicking
+        clicking = not clicking
+
+click_thread = threading.Thread(target = clicker)
+click_thread.start()
+
+with Listener(on_press = toggle_event) as listener:
+    listener.join()
